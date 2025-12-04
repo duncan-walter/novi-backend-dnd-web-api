d28c467dba709cf5f1511b3918f0fff451c0de0d
Duncan Walter <duncan1997@hotmail.nl>
2025-12-04 11:24:10 +0100

    Initialized Spring Boot project with Maven and configured datasource

---
+++ src/main/java/walter/duncan/dndwebapi/DndWebApiApplication.java
@@ -0,0 +1,11 @@
+package walter.duncan.dndwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndWebApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DndWebApiApplication.class, args);
+	}
+}
---
+++ src/test/java/walter/duncan/dndwebapi/DndWebApiApplicationTests.java
@@ -0,0 +1,11 @@
+package walter.duncan.dndwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndWebApiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}

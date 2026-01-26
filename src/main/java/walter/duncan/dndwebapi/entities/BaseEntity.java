033e99878bc5563738f6a229479763dd9c6bdc8c
Duncan Walter <duncan1997@hotmail.nl>
2026-01-26 15:48:06 +0100

    Added id setter to BaseEntity to support mapping sub-entities on a new entity while still being able to make a reference to an existing resource

--- src/main/java/walter/duncan/dndwebapi/entities/BaseEntity.java
+++ src/main/java/walter/duncan/dndwebapi/entities/BaseEntity.java
@@ -24,4 +24,17 @@ public abstract class BaseEntity {
     public Long getId() {
         return this.id;
     }
+
+    /**
+     * Exists to support reattaching existing sub-entities due to mapping restrictions in the
+     * entity > business model > entity mapping flow where persistence identity is lost.
+     * Using this outside persistence mapping is a developer error. Beware.
+     */
+    public void setId(Long id) {
+        if (this.id != null) {
+            throw new IllegalStateException("We found a developer that cannot read!");
+        }
+
+        this.id = id;
+    }
 }

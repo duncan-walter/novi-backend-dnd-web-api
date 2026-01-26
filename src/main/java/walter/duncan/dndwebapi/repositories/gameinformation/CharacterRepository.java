eedfb93c9a464918e4079d595d6109534c501c6c
Duncan Walter <duncan1997@hotmail.nl>
2026-01-26 11:39:13 +0100

    Added CharacterRepository

---
+++ src/main/java/walter/duncan/dndwebapi/repositories/gameinformation/CharacterRepository.java
@@ -0,0 +1,10 @@
+package walter.duncan.dndwebapi.repositories.gameinformation;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import walter.duncan.dndwebapi.entities.charactermanagement.CharacterEntity;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<@NonNull CharacterEntity, @NonNull Long> { }

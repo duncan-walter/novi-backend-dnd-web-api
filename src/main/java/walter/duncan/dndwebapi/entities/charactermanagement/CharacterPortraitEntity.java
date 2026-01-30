af2ceeb06e2024a6583e68a327433b890ec9ed8b
Duncan Walter <duncan1997@hotmail.nl>
2026-01-30 15:00:45 +0100

    Added missing getters and setters to CharacterPortraitEntity

--- src/main/java/walter/duncan/dndwebapi/entities/charactermanagement/CharacterPortraitEntity.java
+++ src/main/java/walter/duncan/dndwebapi/entities/charactermanagement/CharacterPortraitEntity.java
@@ -19,4 +19,38 @@ public final class CharacterPortraitEntity extends BaseEntity {
     @OneToOne
     @JoinColumn(name = "character_id", unique = true, nullable = false)
     private CharacterEntity character;
+
+    //region Getters & Setters
+    public String getOriginalFileName() {
+        return this.originalFileName;
+    }
+
+    public void setOriginalFileName(String originalFileName) {
+        this.originalFileName = originalFileName;
+    }
+
+    public String getStoredFileName() {
+        return this.storedFileName;
+    }
+
+    public void setStoredFileName(String storedFileName) {
+        this.storedFileName = storedFileName;
+    }
+
+    public String getMimeType() {
+        return this.mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public CharacterEntity getCharacter() {
+        return this.character;
+    }
+
+    public void setCharacter(CharacterEntity character) {
+        this.character = character;
+    }
+    //endregion
 }

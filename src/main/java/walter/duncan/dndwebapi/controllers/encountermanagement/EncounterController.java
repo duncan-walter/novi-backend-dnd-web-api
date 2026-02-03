b93e870a1cb3e1a8c8b91f4d1118caee036fde9c
Duncan Walter <duncan1997@hotmail.nl>
2026-02-03 11:21:37 +0100

    Added EncounterController with GET (id), GET (all) and POST endpoints

--- src/main/java/walter/duncan/dndwebapi/controllers/encountermanagement/EncounterController.java
+++ src/main/java/walter/duncan/dndwebapi/controllers/encountermanagement/EncounterController.java
@@ -1,39 +1,52 @@
 package walter.duncan.dndwebapi.controllers.encountermanagement;
 
-import java.util.List;
+import java.util.Set;
 import jakarta.validation.Valid;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import walter.duncan.dndwebapi.helpers.UrlHelper;
+import walter.duncan.dndwebapi.dtos.encountermanagement.EncounterRequestDto;
+import walter.duncan.dndwebapi.dtos.encountermanagement.EncounterResponseDto;
+import walter.duncan.dndwebapi.mappers.encountermanagement.EncounterResponseMapper;
+import walter.duncan.dndwebapi.services.encountermanagement.EncounterService;
 
 @RestController
 @RequestMapping("/encounters")
 public class EncounterController {
+    private final EncounterService encounterService;
+    private final EncounterResponseMapper mapper;
     private final UrlHelper urlHelper;
 
-    public EncounterController(UrlHelper urlHelper) {
+    public EncounterController(
+            EncounterService encounterService,
+            EncounterResponseMapper mapper,
+            UrlHelper urlHelper
+    ) {
+        this.encounterService = encounterService;
+        this.mapper = mapper;
         this.urlHelper = urlHelper;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<@NonNull Object> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(""); // Placeholder
+    public ResponseEntity<@NonNull EncounterResponseDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.mapper.toDto(this.encounterService.findById(id)));
     }
 
     @GetMapping
-    public ResponseEntity<@NonNull List<Object>> get() {
-        return ResponseEntity.ok(List.of()); // Placeholder
+    public ResponseEntity<@NonNull Set<EncounterResponseDto>> get() {
+        return ResponseEntity.ok(this.mapper.toDtos(this.encounterService.findAll()));
     }
 
     @PostMapping
-    public ResponseEntity<@NonNull Object> create(@RequestBody @Valid Object requestDto) {
-        var location = this.urlHelper.getResourceUri(0L); // Placeholder
+    public ResponseEntity<@NonNull EncounterResponseDto> create(@RequestBody @Valid EncounterRequestDto requestDto) {
+        var responseDto = this.mapper.toDto(this.encounterService.create(requestDto));
+        var location = this.urlHelper.getResourceUri(responseDto.id());
 
         return ResponseEntity
                 .created(location)
-                .body(""); // Placeholder
+                .body(responseDto);
     }
 
     @PostMapping("/{id}/participants")
